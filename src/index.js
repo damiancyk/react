45cb1dea1be43aded36d50ee298a601922b315ce
DamianP <damiancyk@gmail.com>
2019-07-01 14:25:02 +0200

    Material-UI

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import TicTacToe from "./components/tictactoe/TicTacToe.js";
-
-// ========================================
+import "./components/tictactoe/TicTacToe.css";
 
 ReactDOM.render(<TicTacToe />, document.getElementById("root"));
